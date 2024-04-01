b6b4c8b5251f14ad2253f5a08bbc5a9a53fbf7a0
¨Alicia <Alicia.Rodriguez.benaches@student.nhlstenden.com>
2024-04-01 18:38:52 +0200

    Edit: changed methods to include commant interface

--- src/main/java/KeyController.java
+++ src/main/java/KeyController.java
@@ -9,7 +9,7 @@ import java.awt.event.KeyAdapter;
  * @version 1.4 2007/07/16 Sylvia Stuurman
  * @version 1.5 2010/03/03 Sylvia Stuurman
  * @version 1.6 2014/05/16 Sylvia Stuurman
-*/
+ */
 
 public class KeyController extends KeyAdapter {
 	private Presentation presentation; // Commands are given to the presentation
