866d448e0e2fa8ea2a824d64b0e069accfcdb71b
¨Alicia <Alicia.Rodriguez.benaches@student.nhlstenden.com>
2024-04-02 20:48:30 +0200

    Edit: made class abstract

--- src/main/java/Style.java
+++ src/main/java/Style.java
@@ -14,45 +14,28 @@ import java.awt.Font;
  * @version 1.6 2014/05/16 Sylvia Stuurman
  */
 
-public class Style {
-	private static Style[] styles; // de styles
-	
-	private static final String FONTNAME = "Helvetica";
-	int indent;
-	Color color;
-	Font font;
-	int fontSize;
-	int leading;
-
-	public static void createStyles() {
-		styles = new Style[5];    
-		// The styles are fixed.
-		styles[0] = new Style(0, Color.red,   48, 20);	// style for item-level 0
-		styles[1] = new Style(20, Color.blue,  40, 10);	// style for item-level 1
-		styles[2] = new Style(50, Color.black, 36, 10);	// style for item-level 2
-		styles[3] = new Style(70, Color.black, 30, 10);	// style for item-level 3
-		styles[4] = new Style(90, Color.black, 24, 10);	// style for item-level 4
-	}
-
-	public static Style getStyle(int level) {
-		if (level >= styles.length) {
-			level = styles.length - 1;
+public abstract class Style {
+	// Factory method to create concrete style instances based on the level
+	public static Style getStyleForLevel(int level) {
+		switch (level) {
+			case 0:
+				return new StyleLevel0();
+			case 1:
+				return new StyleLevel1();
+			case 2:
+				return new StyleLevel2();
+			case 3:
+				return new StyleLevel3();
+			case 4:
+				return new StyleLevel4();
+			default:
+				throw new IllegalArgumentException("Invalid style level: " + level);
 		}
-		return styles[level];
 	}
 
-	public Style(int indent, Color color, int points, int leading) {
-		this.indent = indent;
-		this.color = color;
-		font = new Font(FONTNAME, Font.BOLD, fontSize=points);
-		this.leading = leading;
-	}
-
-	public String toString() {
-		return "["+ indent + "," + color + "; " + fontSize + " on " + leading +"]";
-	}
-
-	public Font getFont(float scale) {
-		return font.deriveFont(fontSize * scale);
-	}
+	abstract String getFontName();
+	abstract Color getColor();
+	abstract int getIndent();
+	abstract int getFontSize();
+	abstract int getLeading();
 }
