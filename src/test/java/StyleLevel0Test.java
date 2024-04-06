eb1ced8d39ab910ad2444f65ffc837eea88599eb
¨Alicia <Alicia.Rodriguez.benaches@student.nhlstenden.com>
2024-04-06 22:24:22 +0200

    Add: level tests implemented successfully

---
+++ src/test/java/StyleLevel0Test.java
@@ -0,0 +1,68 @@
+import org.junit.Test;
+import java.awt.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+public class StyleLevel0Test {
+
+    @Test
+    public void testGetFontName_ExpectEquals() {
+        StyleLevel0 style = new StyleLevel0();
+        assertEquals("Helvetica", style.getFontName());
+    }
+
+    @Test
+    public void testGetFontName_ExpectNotEqual() {
+        StyleLevel0 style = new StyleLevel0();
+        assertNotEquals("Arial", style.getFontName());
+    }
+
+    @Test
+    public void testGetColor_ExpectEquals() {
+        StyleLevel0 style = new StyleLevel0();
+        assertEquals(Color.red, style.getColor());
+    }
+
+    @Test
+    public void testGetColor_ExpectNotEqual() {
+        StyleLevel0 style = new StyleLevel0();
+        assertNotEquals(Color.blue, style.getColor());
+    }
+
+    @Test
+    public void testGetIndent_ExpectEquals() {
+        StyleLevel0 style = new StyleLevel0();
+        assertEquals(0, style.getIndent());
+    }
+
+    @Test
+    public void testGetIndent_ExpectNotEqual() {
+        StyleLevel0 style = new StyleLevel0();
+        assertNotEquals(10, style.getIndent());
+    }
+
+    @Test
+    public void testGetFontSize_ExpectEqual() {
+        StyleLevel0 style = new StyleLevel0();
+        assertEquals(48, style.getFontSize());
+    }
+
+    @Test
+    public void testGetFontSize_ExpectNotEqual() {
+        StyleLevel0 style = new StyleLevel0();
+        assertNotEquals(20, style.getFontSize());
+    }
+
+    @Test
+    public void testGetLeading_ExpectEquals() {
+        StyleLevel0 style = new StyleLevel0();
+        assertEquals(20, style.getLeading());
+    }
+
+    @Test
+    public void testGetLeading_ExpectNotEqual() {
+        StyleLevel0 style = new StyleLevel0();
+        assertNotEquals(10, style.getLeading());
+    }
+}
---
+++ src/test/java/StyleLevel4Test.java
@@ -0,0 +1,68 @@
+import org.junit.Test;
+import java.awt.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+public class StyleLevel4Test {
+
+    @Test
+    public void testGetFontName_ExpectEquals() {
+        StyleLevel4 style = new StyleLevel4();
+        assertEquals("Helvetica", style.getFontName());
+    }
+
+    @Test
+    public void testGetFontName_ExpectNotEqual() {
+        StyleLevel4 style = new StyleLevel4();
+        assertNotEquals("Arial", style.getFontName());
+    }
+
+    @Test
+    public void testGetColor_ExpectEquals() {
+        StyleLevel4 style = new StyleLevel4();
+        assertEquals(Color.black, style.getColor());
+    }
+
+    @Test
+    public void testGetColor_ExpectNotEqual() {
+        StyleLevel4 style = new StyleLevel4();
+        assertNotEquals(Color.blue, style.getColor());
+    }
+
+    @Test
+    public void testGetIndent_ExpectEquals() {
+        StyleLevel4 style = new StyleLevel4();
+        assertEquals(90, style.getIndent());
+    }
+
+    @Test
+    public void testGetIndent_ExpectNotEqual() {
+        StyleLevel4 style = new StyleLevel4();
+        assertNotEquals(70, style.getIndent());
+    }
+
+    @Test
+    public void testGetFontSize_ExpectEquals() {
+        StyleLevel4 style = new StyleLevel4();
+        assertEquals(24, style.getFontSize());
+    }
+
+    @Test
+    public void testGetFontSize_ExpectNotEqual() {
+        StyleLevel4 style = new StyleLevel4();
+        assertNotEquals(30, style.getFontSize());
+    }
+
+    @Test
+    public void testGetLeading_ExpectEquals() {
+        StyleLevel4 style = new StyleLevel4();
+        assertEquals(10, style.getLeading());
+    }
+
+    @Test
+    public void testGetLeading_ExpectNotEqual() {
+        StyleLevel4 style = new StyleLevel4();
+        assertNotEquals(5, style.getLeading());
+    }
+}
