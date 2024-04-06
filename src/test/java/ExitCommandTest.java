fe72da0626e0d0e644fc33a2195727be776f1ed4
¨Alicia <Alicia.Rodriguez.benaches@student.nhlstenden.com>
2024-04-06 21:31:43 +0200

    Add: made exit command test successfully

---
+++ src/test/java/ExitCommandTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
+
+class ExitCommandTest {
+    private Presentation presentation;
+    private ExitCommand exitCommand;
+
+    @BeforeEach
+    void setUp() {
+        presentation = mock(Presentation.class);
+        exitCommand = new ExitCommand(presentation);
+    }
+
+    @Test
+    void execute() {
+        exitCommand.execute();
+        verify(presentation, times(1)).exit(0);
+    }
+}
